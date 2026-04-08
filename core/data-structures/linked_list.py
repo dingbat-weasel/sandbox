c1196e8767fb2943cf061f06147f91c613128a11
dingbat-weasel <isaacrperk@gmail.com>
2026-04-08 07:53:54 -0700

    ll

--- core/data-structures/linked_list.py
+++ core/data-structures/linked_list.py
@@ -17,14 +17,14 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.next = None
+        self.next: Node | None = None
 
 
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
+        self.head: Node | None = new_node
+        self.tail: Node | None = new_node
         self.length = 1
 
     def print_list(self):
@@ -33,6 +33,62 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None or self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
 
-ll1 = LinkedList(4)
-ll1.print_list()
+        self.length += 1
+        return True
+
+    # remove last node and return it
+    # tail to new last node
+    # edge empty
+    # edge one
+    def pop(self):
+        if self.head is None:
+            raise IndexError("Invalid index. Length of list is 0.")
+        elif self.length == 1:
+            temp = self.tail
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        else:
+            temp = self.head
+            pre = self.head
+            while temp.next is not None:
+                pre = temp
+                temp = temp.next
+
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+        return True
+
+
+ll = LinkedList(4)
+ll.append(6)
+ll.append(8)
+ll.append(2)
+ll.print_list()
+print("--")
+ll.prepend(5)
+
+ll.print_list()
